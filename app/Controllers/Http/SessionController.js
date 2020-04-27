514696dadb6d798fe1b5390d9a449b28a832923e
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-04-27 16:51:03 -0300

    fix: Included name in authentication response

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const User = use('App/Models/User')
+
 class SessionController {
   async authenticate ({ request, auth }) {
     const { email, password } = request.all()
-    const token = await auth.attempt(email, password)
-    return token
+    const { name } = await User.findBy('email', email)
+    const { token } = await auth.attempt(email, password)
+    return { token, name }
   }
 }
 
