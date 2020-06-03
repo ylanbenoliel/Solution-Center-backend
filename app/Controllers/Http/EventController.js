df465f826746856af3fc064ab97d31bc3b22f01b
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-06-03 19:37:47 -0300

    feat: create codes based on datetime

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -1,4 +1,17 @@
 'use strict'
+const {
+  parseISO,
+  isSaturday,
+  format,
+  formatDistance,
+  isSameDay,
+  isAfter,
+  isBefore
+} = require('date-fns')
+
+const HOURS_SATURDAY = ['08', '09', '10', '11', '12', '13']
+const HOURS_BUSINESS_DAYS = ['08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21']
+
 const Database = use('Database')
 const Event = use('App/Models/Event')
 
@@ -11,41 +24,101 @@ const Event = use('App/Models/Event')
  */
 class EventController {
   /**
-   * Show a list of all events.
-   * GET events
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async index ({ request, response }) {
+  async index ({ request, response, auth }) {
     try {
       const { date, room } = request.all()
+      const userID = auth.user.id
+      let query = {}
+      let hoursInterval = []
+      const ISODate = parseISO(date)
+
+      if (isSaturday(ISODate)) {
+        hoursInterval = HOURS_SATURDAY
+      } else {
+        hoursInterval = HOURS_BUSINESS_DAYS
+      }
+
       if (room) {
-        const events = Database
-          .select('events.id', 'users.name', 'events.room', 'events.date', 'events.time')
+        query = await Database
+          .select('events.id as event',
+            'users.id as user',
+            'users.name',
+            'events.room',
+            'events.date',
+            'events.time')
           .from('events')
           .innerJoin('users', 'users.id', 'events.user_id')
           .where({ date, room })
-        return events
+      } else {
+        query = await Database
+          .select('events.id as event',
+            'users.id as user',
+            'users.name',
+            'events.room',
+            'events.date',
+            'events.time')
+          .from('events')
+          .innerJoin('users', 'users.id', 'events.user_id')
+          .where({ date })
       }
-      const events = Database
-        .select('events.id', 'users.name', 'events.room', 'events.date', 'events.time')
-        .from('events')
-        .innerJoin('users', 'users.id', 'events.user_id')
-        .where({ date })
-      return events
+
+      const currentDate = new Date(Date.now())
+      let code = ''
+
+      const validEvents = query.flatMap((event) => {
+        const dateString = format(event.date, 'yyyy-MM-dd')
+        const dateTimeString = `${dateString} ${event.time}`
+        const parsedDate = parseISO(dateTimeString)
+        if (isSameDay(parsedDate, currentDate) || isAfter(parsedDate, currentDate)) {
+          const dateTimeDistance = formatDistance(currentDate, parsedDate)
+          const distArray = dateTimeDistance.split(' ')
+          const timeDistArray = distArray.length === 2 ? distArray[0] : distArray[1]
+
+          // eslint-disable-next-line eqeqeq
+          if (userID != event.user) {
+            code = '4'
+            return {
+              ...event,
+              code
+            }
+          }
+
+          if (isBefore(parsedDate, currentDate) || (dateTimeDistance.includes('min') || (dateTimeDistance.includes('hour') && timeDistArray < 6))) {
+            code = '3'
+            return {
+              ...event,
+              code
+            }
+          }
+
+          if (dateTimeDistance.includes('day') || (dateTimeDistance.includes('hour') && timeDistArray >= 6)) {
+            code = '2'
+            return {
+              ...event,
+              code
+            }
+          }
+        }
+
+        return event
+      })
+
+      return response.status(200).send({
+        hoursInterval,
+        validEvents
+      })
     } catch (error) {
       return response
         .status(error.status)
-        .send({ message: 'Ocorreu um erro ao retornar os horários' })
+        .send({ message: 'Ocorreu um erro ao retornar os horários.' })
     }
   }
 
   /**
-   * Create/save a new event.
-   * POST events
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
@@ -75,9 +148,6 @@ class EventController {
   }
 
   /**
-   * Display a single event.
-   * GET events/:id
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
@@ -98,17 +168,11 @@ class EventController {
       if (event.rows.length === 0) {
         return response
           .status(404)
-          .send({ message: 'Nenhum horário marcado' })
+          .send({ message: 'Nenhum horário marcado.' })
       }
-
       return event
     } catch (error) {
-      if (error.name === 'ModelNotFoundException') {
-        return response
-          .status(error.status)
-          .send({ message: 'Nenhum horário marcado' })
-      }
-      return response.status(error.status)
+      return response.status(error.status).send({ message: 'Erro ao buscar horários.' })
     }
   }
 
@@ -126,14 +190,14 @@ class EventController {
       const userID = auth.user.id
 
       const event =
-       await Event.query()
-         .where(
-           {
-             id: eventID,
-             user_id: userID
-           }
-         )
-         .fetch()
+        await Event.query()
+          .where(
+            {
+              id: eventID,
+              user_id: userID
+            }
+          )
+          .fetch()
 
       const jsonEvent = event.toJSON()[0]
       // eslint-disable-next-line eqeqeq
