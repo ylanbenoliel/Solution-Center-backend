40f5f22fbd08348eb418e4e69b0c792b3363e721
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-06-10 12:53:47 -0300

    fix: Send account status in authenticate request

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -6,9 +6,9 @@ const User = use('App/Models/User')
 class SessionController {
   async authenticate ({ request, auth }) {
     const { email, password } = request.all()
-    const { name, is_admin } = await User.findBy('email', email)
+    const { name, is_admin, active } = await User.findBy('email', email)
     const { token } = await auth.attempt(email, password)
-    return { token, name, is_admin }
+    return { token, name, is_admin, active }
   }
 }
 
