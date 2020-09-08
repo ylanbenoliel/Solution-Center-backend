73546a5bbffbcc1fb47251e3d097f5cda2002fa2
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-09-08 17:47:18 -0300

    feat: Create log rules

---
+++ app/Controllers/Http/LogController.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const Log = use('App/Models/Log')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with logs
+ */
+class LogController {
+  /**
+   * Show a list of all logs.
+   * GET logs
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async index ({ request, response, auth }) {
+    try {
+      const userID = auth.user.id
+      const logs = await Log
+        .query()
+        .where('user_id', userID)
+        .orderBy('id', 'desc')
+        .fetch()
+
+      return logs
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ message: 'Erro ao buscar registros.' })
+    }
+  }
+}
+
+module.exports = LogController
---
+++ database/migrations/1599597330935_log_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class LogSchema extends Schema {
+  up () {
+    this.create('logs', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('log')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('logs')
+  }
+}
+
+module.exports = LogSchema
