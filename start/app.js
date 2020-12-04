078229ed815a0ade072e4a094a8fb0903c6cc317
Ylan Benoliel <ylanbenoliel@gmail.com>
2020-12-04 12:55:22 -0300

    dev: Add Mail dependency

--- start/app.js
+++ start/app.js
@@ -17,7 +17,8 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/drive/providers/DriveProvider',
-  '@adonisjs/framework/providers/ViewProvider'
+  '@adonisjs/framework/providers/ViewProvider',
+  '@adonisjs/mail/providers/MailProvider'
 ]
 
 /*
