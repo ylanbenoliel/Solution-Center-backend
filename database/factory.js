4800e701f2b35c3a7c0c85bcf353e9218ac0bdbe
Ylan Benoliel <ylanbenoliel@gmail.com>
2021-02-22 19:17:25 -0300

    feat: Create user factory

--- database/factory.js
+++ database/factory.js
@@ -12,10 +12,21 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+const minCPF = 11111111111
+const maxCPF = 99999999999
+const minPhone = minCPF
+const maxPhone = maxCPF
+
+Factory.blueprint('App/Models/User', (faker) => {
+  return {
+    name: faker.username(),
+    email: faker.email(),
+    password: faker.password(),
+    address: faker.address(),
+    cpf: faker.integer({ min: minCPF, max: maxCPF }),
+    phone: faker.integer({ min: minPhone, max: maxPhone }),
+    rg: faker.integer({ min: 11111111, max: 99999999 })
+  }
+})
