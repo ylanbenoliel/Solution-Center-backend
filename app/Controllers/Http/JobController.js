9b3515774ab76b72308fc6bc3c2fa7cd1d88ee70
Ylan Benoliel <ylanbenoliel@gmail.com>
2021-02-23 17:07:33 -0300

    feat: Create job controller

---
+++ app/Controllers/Http/JobController.js
@@ -0,0 +1,70 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with jobs
+ */
+class JobController {
+  /**
+   * Show a list of all jobs.
+   * GET jobs
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new job.
+   * POST jobs
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+
+  }
+
+  /**
+   * Display a single job.
+   * GET jobs/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update job details.
+   * PUT or PATCH jobs/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a job with id.
+   * DELETE jobs/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = JobController
