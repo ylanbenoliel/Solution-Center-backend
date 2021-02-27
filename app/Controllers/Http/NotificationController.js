3706a120f33bcc26f41f29dc7dee2c63e5aec2d5
Ylan Benoliel <ylanbenoliel@gmail.com>
2021-02-27 13:23:13 -0300

    fix: Search notification by token instead of user

--- app/Controllers/Http/NotificationController.js
+++ app/Controllers/Http/NotificationController.js
@@ -23,16 +23,16 @@ class NotificationController {
       const USER_ID = auth.user.id
       const { token } = request.post()
 
-      const hasToken = await Notification.findBy('user_id', USER_ID)
+      const hasToken = await Notification.findBy('token', token)
       if (!hasToken) {
         await Notification.create({ user_id: USER_ID, token: token })
-        return response.status(200).send({ message: 'Token criado.' })
+        return response.status(201).send({ message: 'Token criado.' })
       }
 
-      hasToken.merge({ token: token })
+      hasToken.merge({ user_id: USER_ID, token: token })
       await hasToken.save()
 
-      return response.status(200).send({ message: 'Token atualizado.' })
+      return response.status(200).send({ message: 'Token de notificação atualizado.' })
     } catch (error) {
       return response
         .status(error.status)
