f32fb6747d96e1782d94a9d6076f71025f38b62c
Ylan Benoliel <ylanbenoliel@gmail.com>
2022-01-03 13:39:34 -0300

    feat: Add pg config

--- config/database.js
+++ config/database.js
@@ -5,10 +5,25 @@ const Env = use('Env')
 
 module.exports = {
 
-  connection: Env.get('DB_CONNECTION', 'mysql'),
+  connection: Env.get('DB_CONNECTION', 'pg'),
 
   mysql: {
     client: 'mysql',
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
+    },
+    pool: {
+      min: 0,
+      max: 3
+    },
+    debug: Env.get('DB_DEBUG', false)
+  },
+  pg: {
+    client: 'pg',
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
